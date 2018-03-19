96d53fe554625f36533c12ced19de0269a8bff57
mrdyuk <dyuk1990@gmail.com>
2018-03-19 21:59:23 +0200

    Lesson 8: home work with @DataProvider

--- src/test/java/pages/LinkedinLoginPage.java
+++ src/test/java/pages/LinkedinLoginPage.java
@@ -19,6 +19,15 @@ public class LinkedinLoginPage extends LinkedinBasePage{
     @FindBy(xpath = "//div[@id='global-alert-queue']//strong[not(text()='')]")
     private WebElement alertMassage;
 
+    @FindBy(id = "session_key-login-error")
+    private WebElement emailErrorMassage;
+
+    @FindBy(id = "session_password-login-error")
+    private WebElement passwordErrorMassage;
+
+    @FindBy(id = "domainSuggestion")
+    private WebElement domainSuggestionMassage;
+
 
     public LinkedinLoginPage(WebDriver driver) {
         super(driver);
@@ -27,5 +36,17 @@ public class LinkedinLoginPage extends LinkedinBasePage{
 
     public boolean isNotSignedIn() {
         return alertMassage.isDisplayed();
+    }
+
+    public String getEmailErrorMassage() {
+        return emailErrorMassage.getText();
+    }
+
+    public String getPasswordErrorMassage() {
+        return passwordErrorMassage.getText();
+    }
+
+    public boolean isDomainSuggestionDisplayed() {
+        return domainSuggestionMassage.isDisplayed();
     }
 }
--- src/test/java/tests/LinkedInLoginTest.java
+++ src/test/java/tests/LinkedInLoginTest.java
@@ -7,11 +7,17 @@ import pages.LinkedinLoginPage;
 
 public class LinkedInLoginTest extends LinkedinBaseTest{
 
-    @Test
-    public void successfulLoginTest() {
+    @DataProvider
+    public Object[][] successfulCredentialsLoginTest(){
+        return new Object[][]{
+                {"ITEATEST@I.UA", "1q2w3e_4r5t"},
+                {"iteatest@i.ua", "1q2w3e_4r5t"}};
+    }
+    @Test(dataProvider = "successfulCredentialsLoginTest")
+    public void successfulLoginTest(String email, String password) {
         Assert.assertEquals(initialPageTitle, "LinkedIn: Log In or Sign Up", "Login page title is wrong");
 
-        LinkedinHomePage linkedinHomePage = linkedinLandingPage.loginAs("iteatest@i.ua", "1q2w3e_4r5t");
+        LinkedinHomePage linkedinHomePage = linkedinLandingPage.loginAs(email, password);
         Assert.assertTrue(linkedinHomePage.isSighedIn(), "User is not signed in");
 
         Assert.assertNotEquals(linkedinLandingPage.getPageTitle(), initialPageTitle, "Page title did not change after login");
@@ -21,9 +27,10 @@ public class LinkedInLoginTest extends LinkedinBaseTest{
     @DataProvider
     public Object[][] negativeTestCredentialsReturnToLanding(){
         return new Object[][]{
-                {"", ""}};
+                {"", ""},
+                {"iteatest@i.ua", ""},
+                {"", "1q2w3e_4r5t"}};
     }
-
     @Test(dataProvider = "negativeTestCredentialsReturnToLanding")
     public void negativeTestCredentialsReturnToLanding(String email, String password) {
         linkedinLandingPage.loginAs(email, password);
@@ -33,15 +40,41 @@ public class LinkedInLoginTest extends LinkedinBaseTest{
     @DataProvider
     public Object[][] negativeTestCredentialsReturnToLoginPage(){
         return new Object[][]{
-                {"xyzxyz", "xyz", "Please enter a valid email address", "The password you provided must have at least 6 characters"}};
+                {"xyzxyz", "xyz", "Please enter a valid email address.", "The password you provided must have at least 6 characters."}};
     }
-
     @Test(dataProvider = "negativeTestCredentialsReturnToLoginPage")
     public void negativeTestCredentialsReturnToLoginPage(String email, String password, String emailAlert, String passwordAlert) {
         LinkedinLoginPage linkedinLoginPage = linkedinLandingPage.loginAs(email, password);
         Assert.assertTrue(linkedinLoginPage.isNotSignedIn(), "Alert massage is not displayed");
         Assert.assertNotEquals(linkedinLoginPage.getPageTitle(), initialPageTitle, "Page title did not change after login");
+        Assert.assertEquals(linkedinLoginPage.getEmailErrorMassage(), emailAlert, "Email alert message is not displayed");
+        Assert.assertEquals(linkedinLoginPage.getPasswordErrorMassage(), passwordAlert, "Password alert message is not displayed");
+    }
 
-        //создать 2 метода для возврата  String emailAlert, String passwordAlert и вссунуть их в ассерты
+    @DataProvider
+    public Object[][] negativeLoginTestWithoutRightPassword(){
+        return new Object[][]{
+                {"iteatest@i.ua", "xyzzyx", "Hmm, that's not the right password. Please try again or request a new one."}};
+    }
+    @Test(dataProvider = "negativeLoginTestWithoutRightPassword")
+    public void negativeLoginTestWithoutRightPassword(String email, String password, String passwordAlert) {
+        LinkedinLoginPage linkedinLoginPage = linkedinLandingPage.loginAs(email, password);
+        Assert.assertTrue(linkedinLoginPage.isNotSignedIn(), "Alert massage is not displayed");
+        Assert.assertNotEquals(linkedinLoginPage.getPageTitle(), initialPageTitle, "Page title did not change after login");
+        Assert.assertEquals(linkedinLoginPage.getPasswordErrorMassage(), passwordAlert, "Password alert message is not displayed");
+    }
+
+    @DataProvider
+    public Object[][] negativeTestWithDomainSuggestionMessage(){
+        return new Object[][]{
+                {"iteatest@i.u", "1q2w3e_4r5t"}};
+    }
+    @Test(dataProvider = "negativeTestWithDomainSuggestionMessage")
+    public void negativeTestWithDomainSuggestionMessage(String email, String password) {
+        LinkedinLoginPage linkedinLoginPage = linkedinLandingPage.loginAs(email, password);
+        Assert.assertTrue(linkedinLoginPage.isNotSignedIn(), "Alert massage is not displayed");
+        Assert.assertNotEquals(linkedinLoginPage.getPageTitle(), initialPageTitle, "Page title did not change after login");
+        Assert.assertTrue(linkedinLoginPage.isDomainSuggestionDisplayed(), "Domain suggestion message is not displayed");
+
     }
 }
