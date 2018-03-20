bc46bf0efd6b7acfc2e9d688b81f59beba4fbd33
mrserdyuk <dyuk1990@gmail.com>
2018-03-20 22:01:51 +0200

    Lesson 9: run tests with .xml file in different browsers

--- src/test/java/tests/LinkedinBaseTest.java
+++ src/test/java/tests/LinkedinBaseTest.java
@@ -1,9 +1,13 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import pages.LinkedinLandingPage;
 
 public class LinkedinBaseTest {
@@ -12,9 +16,22 @@ public class LinkedinBaseTest {
     String initialPageUrl;
     String initialPageTitle;
 
+
+    @Parameters({"browserType"})
     @BeforeMethod
-    public void beforeTest() {
-        driver = new FirefoxDriver();
+    public void beforeTest(@Optional("firefox") String browserType) {
+
+        if(browserType.toLowerCase().equals("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        else if(browserType.toLowerCase().equals("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else
+            System.out.println("Unsupported browser");
+
         driver.navigate().to("https://www.linkedin.com/");
         linkedinLandingPage = new LinkedinLandingPage(driver);
 
